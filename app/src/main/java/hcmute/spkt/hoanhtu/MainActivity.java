9f7adf5d4159d0da47dff36946e5629f0a18bac1
trong.tn178@gmail.com <trong.tn178@gmail.com>
2020-04-05 10:58:32 +0700

    [trong] show numeric to edittext screen

--- app/src/main/java/hcmute/spkt/hoanhtu/MainActivity.java
+++ app/src/main/java/hcmute/spkt/hoanhtu/MainActivity.java
@@ -3,40 +3,113 @@ package hcmute.spkt.hoanhtu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText edScreen;
-    private TextView num1,num2,num3,num4,num5,num6,num7,num8,num9,num0;
-    private TextView dot,cong,tru,nhan,chia,bang,txtC;
+    public EditText edScreen;
+    public Button num1, num2, num3, num4, num5, num6, num7, num8, num9, num0;
+    public Button dot, plus, minus, multiply, division, equal, clear;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         findID();
-        
+        setNumericOnClickListener();
+        setOperationOnClickListener();
     }
 
-    private void findID()
-    {
-        edScreen=(EditText)findViewById(R.id.screen);
-        num0=(TextView)findViewById(R.id.num0);
-        num1=(TextView)findViewById(R.id.num1);
-        num2=(TextView)findViewById(R.id.num2);
-        num3=(TextView)findViewById(R.id.num3);
-        num4=(TextView)findViewById(R.id.num4);
-        num5=(TextView)findViewById(R.id.num5);
-        num6=(TextView)findViewById(R.id.num6);
-        num7=(TextView)findViewById(R.id.num7);
-        num8=(TextView)findViewById(R.id.num8);
-        num9=(TextView)findViewById(R.id.num9);
-        dot=(TextView)findViewById(R.id.dot);
-        cong=(TextView)findViewById(R.id.sumation);
-        tru=(TextView)findViewById(R.id.subtraction);
-        nhan=(TextView)findViewById(R.id.multiplication);
-        chia=(TextView)findViewById(R.id.division);
+    private void findID() {
+        edScreen = (EditText) findViewById(R.id.screen);
+        edScreen.setText("con me no");
+        num0 = (Button) findViewById(R.id.num0);
+
+        num1 = (Button) findViewById(R.id.num1);
+        num2 = (Button) findViewById(R.id.num2);
+        num3 = (Button) findViewById(R.id.num3);
+        num4 = (Button) findViewById(R.id.num4);
+        num5 = (Button) findViewById(R.id.num5);
+        num6 = (Button) findViewById(R.id.num6);
+        num7 = (Button) findViewById(R.id.num7);
+        num8 = (Button) findViewById(R.id.num8);
+        num9 = (Button) findViewById(R.id.num9);
+        dot = (Button) findViewById(R.id.dot);
+        plus = (Button) findViewById(R.id.sumation);
+        minus = (Button) findViewById(R.id.subtraction);
+        multiply = (Button) findViewById(R.id.multiplication);
+        division = (Button) findViewById(R.id.division);
+        equal = (Button) findViewById(R.id.equal);
+        clear = (Button) findViewById(R.id.clear);
+    }
+
+
+    private void setNumericOnClickListener() {
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    Button btn = (Button) view;
+                    System.out.println(btn.getText());
+                    edScreen.append(btn.getText().toString());
+                } catch (Exception e) {
+
+                    System.out.println(61 + " " + e.getMessage());
+                }
+            }
+        };
+        findViewById(R.id.num0).setOnClickListener(listener);
+        findViewById(R.id.num1).setOnClickListener(listener);
+        findViewById(R.id.num2).setOnClickListener(listener);
+        findViewById(R.id.num3).setOnClickListener(listener);
+        findViewById(R.id.num4).setOnClickListener(listener);
+        findViewById(R.id.num5).setOnClickListener(listener);
+        findViewById(R.id.num6).setOnClickListener(listener);
+        findViewById(R.id.num7).setOnClickListener(listener);
+        findViewById(R.id.num8).setOnClickListener(listener);
+        findViewById(R.id.num9).setOnClickListener(listener);
+    }
+
+    private void setOperationOnClickListener() {
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!edScreen.getText().toString().isEmpty()) {
+                    if (edScreen.getText().toString().contains("+")
+                            || edScreen.getText().toString().contains("-")
+                            || edScreen.getText().toString().contains("*")
+                            || edScreen.getText().toString().contains("/")) {
+                        System.out.println("nothing");
+                    } else {
+                        Button button = (Button) view;
+                        edScreen.append(button.getText());
+                    }
+                }
+            }
+        };
+
+//        plus = (Button) findViewById(R.id.sumation);
+//        minus = (Button) findViewById(R.id.subtraction);
+//        multiply = (Button) findViewById(R.id.multiplication);
+//        division = (Button) findViewById(R.id.division);
+        findViewById(R.id.sumation).setOnClickListener(listener);
+        findViewById(R.id.subtraction).setOnClickListener(listener);
+        findViewById(R.id.multiplication).setOnClickListener(listener);
+        findViewById(R.id.division).setOnClickListener(listener);
+
+        findViewById(R.id.clear).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                edScreen.setText("");
+            }
+        });
 
     }
+
+
 }
